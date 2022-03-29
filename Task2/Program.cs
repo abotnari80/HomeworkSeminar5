5b5ab455149292479b76461ac289cee92410ebb3
abotnari80 <abotnari80@gmail.com>
2022-03-29 23:51:55 +0300

    Задача 2

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿int result = 0;
+int[] array = new int[4];
+
+for (int i = 0; i < 4; i++)
+{
+    array[i] = new Random().Next(-100, 100);
+    System.Console.Write($"{array[i]} ");
+}
+
+System.Console.WriteLine();
+
+for (int i = 0; i < 4; i++)
+{
+    if ((i + 1) % 2 != 0)
+    {
+        result = result + array[i];
+    }
+}
+
+System.Console.WriteLine(result);
