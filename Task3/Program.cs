332ad7492631d586fdb40e1779c81c46b1e3cb85
abotnari80 <abotnari80@gmail.com>
2022-03-29 23:58:37 +0300

    Задача 3

---
+++ Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿int result = 0;
+int minValue = 101;
+int maxValue = 0;
+int[] array = new int[4];
+
+for (int i = 0; i < 4; i++)
+{
+    array[i] = new Random().Next(-100, 100);
+    System.Console.Write($"{array[i]} ");
+}
+
+System.Console.WriteLine();
+
+for (int i = 0; i < 4; i++)
+{
+    if (maxValue < array[i])
+    {
+        maxValue = array[i];
+    }
+    
+    if (minValue > array[i])
+    {
+        minValue = array[i];
+    }
+}
+
+result = maxValue - minValue;
+System.Console.WriteLine(result);
