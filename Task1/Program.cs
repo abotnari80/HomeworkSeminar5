5f75582103536488b47f869546a598008b7c8926
abotnari80 <abotnari80@gmail.com>
2022-03-29 23:40:53 +0300

    Задача 1

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿int result = 0;
+int arrayPart = 0;
+int[] array = new int[4];
+
+for (int i = 0; i < 4; i++)
+{
+    array[i] = new Random().Next(100, 999);
+    System.Console.Write($"{array[i]} ");
+}
+
+System.Console.WriteLine();
+
+for (int i = 0; i < 4; i++)
+{
+    if (array[i] > arrayPart)
+    {
+        arrayPart = array[i];
+        result = i + 1;
+    }
+}
+
+System.Console.WriteLine(result);
